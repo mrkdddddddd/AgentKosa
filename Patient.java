14ffa799141f79816aab374ae55009b04bd55d12
MYAIR <ymy0613@gmail.com>
2025-03-17 09:09:40 +0900

    generate Patient

---
+++ Patient.java
@@ -0,0 +1,21 @@
+public class Patient {
+
+    private String pName;
+   
+    public Patient(){}
+
+    public Patient (String pName){
+        this.pName = pName;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    
+
+}
